32992a2e77ea5f307f50f20f82660fe173a3576b
Tata <tatajavvadi@gmail.com>
2025-09-16 18:58:00 +0530

    Base Skeleton setup

---
+++ src/main/java/com/metro_ticket_booking/metro_ticket_booking/MetroTicketBookingApplication.java
@@ -0,0 +1,13 @@
+package com.metro_ticket_booking.metro_ticket_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroTicketBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroTicketBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metro_ticket_booking/metro_ticket_booking/MetroTicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metro_ticket_booking.metro_ticket_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroTicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
